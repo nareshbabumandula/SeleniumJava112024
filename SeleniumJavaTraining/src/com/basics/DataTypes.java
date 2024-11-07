2aa08c30bcca60c656413143fb9574e7e70a9d60
[nareshbabumandula] <naresh223@gmail.com>
2024-11-07 22:36:17 +0530

    07-Nov-2024: Naresh: Added print statement for all the primitive datatype variables

--- SeleniumJavaTraining/src/com/basics/DataTypes.java
+++ SeleniumJavaTraining/src/com/basics/DataTypes.java
@@ -9,12 +9,16 @@ public class DataTypes {
 		int i=53455;
 		long l=85463;
 		float f=12.6f;
-	    double d=634.3646;
-	    boolean bFlag=true;
-	    char c='n';
-	    System.out.println("Byte value is : " + b);
-	    System.out.println("Short value is : " + s);
-	    
+		double d=634.3646;
+		boolean bFlag=true;
+		char c='n';
+		System.out.println("Byte value is : " + b);
+		System.out.println("Short value is : " + s);
+		System.out.println("Integer value is : " + i);
+		System.out.println("Long value is : " + l);
+		System.out.println("Float value is : " + f);
+		System.out.println("Double value is : " + d);
+		System.out.println("Boolean value is : " + bFlag);
+		System.out.println("Char value is : " + c);
 	}
-
 }
