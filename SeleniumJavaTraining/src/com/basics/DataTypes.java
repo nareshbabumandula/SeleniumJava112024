4981b10686d4bcba356aa7e3bd6e9a8cddeabe10
[nareshbabumandula] <naresh223@gmail.com>
2024-11-07 22:13:38 +0530

    Selenium Project

---
+++ SeleniumJavaTraining/src/com/basics/DataTypes.java
@@ -0,0 +1,20 @@
+package com.basics;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// Primitive (important) data types
+		byte b=109;
+		short s=1242;
+		int i=53455;
+		long l=85463;
+		float f=12.6f;
+	    double d=634.3646;
+	    boolean bFlag=true;
+	    char c='n';
+	    System.out.println("Byte value is : " + b);
+	    System.out.println("Short value is : " + s);
+	    
+	}
+
+}
