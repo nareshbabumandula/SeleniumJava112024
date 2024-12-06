940811c11c135e751ef537e5e2589d3233690525
[nareshbabumandula] <naresh223@gmail.com>
2024-12-06 21:40:59 +0530

    Test Commit

--- SeleniumJavaTraining/src/com/exceptions/CheckedExceptions.java
+++ SeleniumJavaTraining/src/com/exceptions/CheckedExceptions.java
@@ -3,7 +3,7 @@ package com.exceptions;
 public class CheckedExceptions {
 
 	public static void main(String[] args) {
-		System.out.println("Checked exceptions test..!");
+		System.out.println("Checked exceptions Test..!");
 	}
 
 }
