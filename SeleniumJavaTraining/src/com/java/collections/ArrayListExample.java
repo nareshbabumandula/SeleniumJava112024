049e437846d0ce997a51142dd2588a3ef69eec9a
Dell <Dell@DESKTOP-UTBJ5VR>
2024-12-06 22:09:09 +0530

    Added one object to ArrayList collection

--- SeleniumJavaTraining/src/com/java/collections/ArrayListExample.java
+++ SeleniumJavaTraining/src/com/java/collections/ArrayListExample.java
@@ -13,6 +13,7 @@ public class ArrayListExample {
 		alist.add("Aarthi");
 		alist.add("Preethi");
 		alist.add("Sai");
+		alist.add("Narmada");
 		alist.add("Naresh");
 		alist.add("Sowjanya");
 		alist.remove(0);
