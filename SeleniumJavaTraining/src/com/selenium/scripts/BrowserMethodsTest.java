e886d358bfecfd040b68952c383373fdc4b4fdc4
[nareshbabumandula] <naresh223@gmail.com>
2024-12-18 22:39:29 +0530

    testNG Introduction

--- SeleniumJavaTraining/src/com/selenium/scripts/BrowserMethodsTest.java
+++ SeleniumJavaTraining/src/com/selenium/scripts/BrowserMethodsTest.java
@@ -3,22 +3,60 @@ package com.selenium.scripts;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Reporter;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
 public class BrowserMethodsTest {
+
+	// WebDriver is a remote control interface that enables introspection and control of user agents (browsers).
+	WebDriver driver;
+
+	/**
+	 * @BeforeTest: The annotated method will be run before any test method belonging to the classes inside the <test> tag is run.
+	 */
+	@Parameters({"browserType"})
+	@BeforeTest
+	void accessSite(String browser) {
+		switch (browser) {
+		case "chrome": case "google":
+			driver = new ChromeDriver();
+			break;
+		case "firefox": case "mozilla":
+			driver = new FirefoxDriver();
+			break;
+		case "edge": case "chromium":
+			driver = new EdgeDriver();
+			break;
+		default:
+			break;
+		}
 		
-	public static void main(String[] args) throws InterruptedException {
-		// WebDriver is a remote control interface that enables introspection and control of user agents (browsers).
-		WebDriver driver = new ChromeDriver();
+		
 		driver.get("https://www.mycontactform.com");
 		driver.manage().window().maximize();
+	}
+
+	@Test
+	void browserMethods() throws InterruptedException {
 		String title = driver.getTitle();
 		System.out.println("Page title is : " + title);
 		if (title.contains("Contact and Email Forms")) {
 			System.out.println("Page title is valid");
+			Reporter.log("Page title is valid");
 		} else {
 			System.out.println("Page title is invalid");
+			Reporter.log("Page title is invalid");
 		}
 		System.out.println(driver.getCurrentUrl());
+	}
+
+	@Test
+	void browserNavigationMethods() throws InterruptedException {
 		driver.findElement(By.linkText("Sample Forms")).click();
 		driver.navigate().back();
 		Thread.sleep(2000);
@@ -27,9 +65,16 @@ public class BrowserMethodsTest {
 		driver.navigate().refresh();
 		System.out.println(driver.getWindowHandle());
 		System.out.println(driver.getPageSource());
+	}
+
+	/**
+	 * @AfterTest: The annotated method will be run after all the test methods belonging to the classes inside the <test> tag have run.
+	 * @throws InterruptedException
+	 */
+	@AfterTest
+	void closeBrowser() throws InterruptedException {
 		Thread.sleep(3000);
 		driver.quit();
-
 	}
 
 }
